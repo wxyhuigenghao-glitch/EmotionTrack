e41dccb929331266b9bf47db66c6cae0b0d99b40
Xinyu Weng <wxyhuigenghao@163.com>
2026-08-18 00:31:42 +0800

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/affective-context-game/",
+  base: "/EmotionTrack/",
   build: {
     outDir: "dist",
     sourcemap: true,
